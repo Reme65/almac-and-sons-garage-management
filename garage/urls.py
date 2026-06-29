316f0d4bb6e597693d2f9063724ce3040602e105
Reme65 <amreme168@gmail.com>
2026-06-29 21:54:19 +0100

    Connect static files and images in Django templates

--- garage/urls.py
+++ garage/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("services/", views.services, name="services"),
+    path("booking/", views.booking, name="booking"),
+    path("thank-you/", views.thank_you, name="thank_you"),
 ]
--- garage/views.py
+++ garage/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "index.html")
+
+
+def services(request):
+    return render(request, "services.html")
+
+
+def booking(request):
+    return render(request, "booking.html")
+
+
+def thank_you(request):
+    return render(request, "thank-you.html")
