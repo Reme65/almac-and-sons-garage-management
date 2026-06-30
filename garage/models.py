deb7eda6b2f6a1d35a2df490720c0f78abe78bd4
Reme65 <amreme168@gmail.com>
2026-06-30 19:28:40 +0100

    Add Booking model and configure Django admin

--- garage/models.py
+++ garage/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Booking(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    vehicle_registration = models.CharField(max_length=20)
+    booking_date = models.DateField()
+    service_required = models.CharField(max_length=100)
+    message = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.vehicle_registration}"
