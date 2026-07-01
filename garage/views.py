1612158880edac42ab36624d000a640687a96fd3
Reme65 <amreme168@gmail.com>
2026-07-01 20:57:42 +0100

    Connect booking form to database

--- garage/views.py
+++ garage/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Booking
 
 
 def home(request):
@@ -10,6 +11,25 @@ def services(request):
 
 
 def booking(request):
+    if request.method == "POST":
+        full_name = request.POST.get("name", "")
+        name_parts = full_name.split(" ", 1)
+
+        first_name = name_parts[0]
+        last_name = name_parts[1] if len(name_parts) > 1 else ""
+
+        Booking.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            email=request.POST.get("email"),
+            phone=request.POST.get("phone"),
+            vehicle_registration=request.POST.get("reg"),
+            booking_date=request.POST.get("date"),
+            service_required=request.POST.get("service"),
+        )
+
+        return redirect("thank_you")
+
     return render(request, "booking.html")
 
 
