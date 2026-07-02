7b7425db7fe81a5052ec75bf3c240c26dfbf763d
Reme65 <amreme168@gmail.com>
2026-07-02 15:22:17 +0100

    Improve booking admin interface

--- garage/admin.py
+++ garage/admin.py
@@ -2,4 +2,26 @@ from django.contrib import admin
 from .models import Booking
 
 
-admin.site.register(Booking)
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "first_name",
+        "last_name",
+        "vehicle_registration",
+        "service_required",
+        "booking_date",
+    )
+
+    search_fields = (
+        "first_name",
+        "last_name",
+        "vehicle_registration",
+    )
+
+    list_filter = (
+        "service_required",
+        "booking_date",
+    )
+
+    ordering = ("booking_date",)
